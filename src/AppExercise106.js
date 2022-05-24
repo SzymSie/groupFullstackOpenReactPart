6003948064631ad3e5489927c7c59a07af200614
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-05-24 14:47:39 -0400

    1.7 finished

--- src/AppExercise106.js
+++ src/AppExercise106.js
@@ -5,29 +5,45 @@ const AppExercise106 = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  //
-  // }
+  const handleClick = (input) => {
+    setAll(all + 1)
+    if (input === 'good') {
+      setGood(good + 1)
+    }if (input === 'neutral') {
+      setNeutral(neutral + 1)
+    }if (input === 'bad') {
+      setBad(bad + 1)
+    }
+  }
+  
+  let average = ((good*1 + neutral*0 + bad*-1) / all)
+  let positive = ((good*1 / all))
 
   return (
     <div>
       <h2>give feedback</h2>
       
-      <button onClick={() => {setGood(good + 1)}}>good</button>
-      <button onClick={() => {setNeutral(neutral + 1)}}>neutral</button>
-      <button onClick={() => {setBad(bad + 1)}}>bad</button>
+      <button onClick={() => handleClick("good")}>good</button>
+      <button onClick={() => handleClick("neutral")}>neutral</button>
+      <button onClick={() => handleClick("bad")}>bad</button>
 
       <h2>statistics</h2>
 
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+
+      {/* 1.6 */}
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
       
     </div>
   )
 }
 
 export default AppExercise106
-
-// onClick(() => setGood())
-//
+// average (good + neutral + bad) / 
+//  (good: 1, neutral: 0, bad: -1) 
