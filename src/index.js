3a3318b73ab1eab49223d9e03c986d6859ca88ab
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-05-26 15:09:31 -0400

    vitals removed for codesandbox

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './Apps/App';
 import AppExercise106 from './Apps/AppExercise106';
 import AppExercise112 from './Apps/AppExercise112';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +13,3 @@ root.render(
     <AppExercise112 />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
