53bad548274695619531728bb868d495e16fb90a
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-05-31 13:38:07 -0400

    2022.05.31
    113 finished

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './Apps/App';
 import AppExercise106 from './Apps/AppExercise106';
 import AppExercise112 from './Apps/AppExercise112';
+import AppExercise113 from './Apps/AppExercise113';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <AppExercise106 /> */}
-    <AppExercise112 />
+    {/* <AppExercise112 /> */}
+    <AppExercise113 />
   </React.StrictMode>
 );
