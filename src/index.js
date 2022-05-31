dc9ac9c0aaf334ec51410c49c1398b111900486e
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-05-31 14:56:44 -0400

    1.14 finished
    1.14 finished

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './Apps/App';
 import AppExercise106 from './Apps/AppExercise106';
 import AppExercise112 from './Apps/AppExercise112';
 import AppExercise113 from './Apps/AppExercise113';
+import AppExercise114 from './Apps/AppExercise114';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
     {/* <App /> */}
     {/* <AppExercise106 /> */}
     {/* <AppExercise112 /> */}
-    <AppExercise113 />
+    {/* <AppExercise113 /> */}
+    <AppExercise114 />
   </React.StrictMode>
 );
