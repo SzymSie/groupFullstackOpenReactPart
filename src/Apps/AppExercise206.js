1b43ea82d58e6b945a7991e3ba74b1d0d88d1eba
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-06-09 14:36:48 -0400

    206 finished

--- src/Apps/AppExercise206.js
+++ src/Apps/AppExercise206.js
@@ -2,23 +2,35 @@ import { useState } from 'react'
 
 const AppExercise206 = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' },
   ])
   const [newName, setNewName] = useState('')
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault()
+    // setPersons(prevState => {
+    //   prevState.concat({name: newName})
+    // })
+    setPersons(persons.concat({name: newName}))
+  }
+
+  const onChangeHandler = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={onChangeHandler} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={onSubmitHandler}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        {persons.map(person => <div key={person.name}>{person.name}</div>)}
     </div>
   )
 }
