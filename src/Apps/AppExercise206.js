a6adf16707d6446b65b1e6086149f20a34c341df
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-06-14 13:50:19 -0400

    2022.06.14 try 207

--- src/Apps/AppExercise206.js
+++ src/Apps/AppExercise206.js
@@ -1,22 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const AppExercise206 = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-  ])
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
 
   const onSubmitHandler = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // setPersons(prevState => {
     //   prevState.concat({name: newName})
     // })
-    setPersons(persons.concat({name: newName}))
-  }
+    //   setPersons(persons.concat({name: newName}))
+
+    if (persons.some((person) => person.name === newName.name)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    setPersons((personsArr) => personsArr.concat({ name: newName }));
+  };
 
   const onChangeHandler = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
@@ -26,13 +30,17 @@ const AppExercise206 = () => {
           name: <input onChange={onChangeHandler} />
         </div>
         <div>
-          <button type="submit" onClick={onSubmitHandler}>add</button>
+          <button type="submit" onClick={onSubmitHandler}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(person => <div key={person.name}>{person.name}</div>)}
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default AppExercise206;
