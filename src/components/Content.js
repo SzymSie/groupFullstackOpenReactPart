10748103bdbdb59670b85861c38985c26bb44d26
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-06-15 12:40:22 -0400

    Updated

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,23 +1,23 @@
-    import { Part } from "./Part"
+export function Content(props) {
+    // console.log({children})
+    console.log(props.children)
+    return (
+      <div>
+        {props.children}        
+      </div>
+    );
+  }
 
-    export function Content(props) {
-      
-      
-      return (
-        <div> 
-          <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises}/>
-          <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises}/>
-          <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises}/>
-          {/* <p >
-            {props.part1} {props.exercise1}
-          </p>
-          <p>
-            {props.part2} {props.exercise2}
-          </p>
-          <p>
-            {props.part3} {props.exercise3}
-          </p> */}
-        </div>
-      )
-    }
-    // props.course.parts[0].exercises
+  // <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises}/>
+  // <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises}/>
+  // <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises}/>
+          
+  // <p >
+  //             {props.part1} {props.exercise1}
+  //           </p>
+  //           <p>
+  //             {props.part2} {props.exercise2}
+  //           </p>
+  //           <p>
+  //             {props.part3} {props.exercise3}
+  //           </p>
