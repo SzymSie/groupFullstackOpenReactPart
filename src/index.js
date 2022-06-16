19264fee9aff9893bf5cd8f93c4338b3895053ab
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2022-06-16 19:33:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppExercise101 from "./Apps/AppExercise101";
+// import AppExercise101 from "./Apps/AppExercise101";
 import AppExercise106 from "./Apps/AppExercise106";
 import AppExercise112 from "./Apps/AppExercise112";
 import AppExercise113 from "./Apps/AppExercise113";
@@ -13,7 +13,7 @@ import AppExercise206 from "./Apps/AppExercise206";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppExercise101 />
+    {/* <AppExercise101 /> */}
     {/* <AppExercise106 /> */}
     {/* <AppExercise112 /> */}
     {/* <AppExercise113 /> */}
